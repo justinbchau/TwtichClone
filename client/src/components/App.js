0995b92153e1e3ba1b51ee506e73c73a9d3f25a8
mrchaufun <46144954+mrchaufun@users.noreply.github.com>
2020-02-21 06:29:41 -0800

    Added StreamForm page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, } from 'react-router-dom';
+import { Router, Route, } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
@@ -7,20 +7,21 @@ import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
     return (
         <div className="ui container" >
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/edit/:id" component={StreamEdit} />
                     <Route path="/streams/delete" exact component={StreamDelete} />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        const id = this.props.match.params.id
+        this.props.fetchStream(id);
+    }
+
+
+    render() {
+        if (!this.props.stream) {
+            return <div>...Loading</div>
+        }
     return (
         <div>
-            StreamEdit
+            {this.props.stream.title}
         </div>
     );
+   };
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
