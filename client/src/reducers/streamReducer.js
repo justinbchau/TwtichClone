98fb5b7ffb72344d715514749afeaaf7b15574b8
mrchaufun <46144954+mrchaufun@users.noreply.github.com>
2020-02-20 21:25:22 -0800

    route added to create stream page when user is signed in

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -8,14 +9,16 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload };
-
-
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
