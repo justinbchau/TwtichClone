174f7e61bc927f6a67dd300a66c189b68be59f41
mrchaufun <justinchau31@gmail.com>
2020-02-22 12:47:21 -0800

    Creating Modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Modal from '../Modal';
 
 const StreamDelete = () => {
     return (
         <div>
             StreamDelete
+            <Modal />
         </div>
     );
 };
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,13 +1,18 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
 
 class StreamEdit extends React.Component {
     componentDidMount() {
-        const id = this.props.match.params.id
-        this.props.fetchStream(id);
+        this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
 
     render() {
         if (!this.props.stream) {
@@ -15,14 +20,17 @@ class StreamEdit extends React.Component {
         }
     return (
         <div>
-            {this.props.stream.title}
+            <h3>Edit a Stream</h3>
+            <StreamForm 
+             initialValues={_.pick(this.props.stream, 'title', 'description')}
+             onSubmit={this.onSubmit} />
         </div>
     );
-   };
-};
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
